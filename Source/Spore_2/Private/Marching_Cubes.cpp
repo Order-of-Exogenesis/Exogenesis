cd154efd84fff7a790b0c30fe65081c77f0cd0e4
Quintessence <82589146+offfelldsshift@users.noreply.github.com>
2025-10-23 20:45:19 +0700

    Massive Edit

--- Source/Spore_2/Private/Marching_Cubes.cpp
+++ Source/Spore_2/Private/Marching_Cubes.cpp
@@ -2,38 +2,65 @@
 
 // Marching_Cubes.cpp
 #include "Marching_Cubes.h"
-#include "Components/StaticMeshComponent.h"
-#include "UObject/ConstructorHelpers.h"
+#include "Kismet/KismetMathLibrary.h"
 
 
-AMarching_Cubes::AMarching_Cubes()
+void UMarching_Cubes::GenerateMesh(UProceduralMeshComponent* MeshComponent, float Radius, int Resolution)
 {
-	PrimaryActorTick.bCanEverTick = false;
+    // Очистка существующего меша компонента
+    MeshComponent->ClearMesh();
 
+    // Вычисляем размер каждого куба в алгоритме Marching Cubes
+    float cubeSize = Radius / Resolution;
 
-	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
-	RootComponent = MeshComponent;
+    //Массивы для хранения вершин, треугольников, нормалей и т.д.
+    TArray<FVector> Vertices;
+    TArray<int32> Triangles;
+    TArray<FVector> Normals;
+    TArray<FVector2D> UVs;
+    TArray<FLinearColor> VertexColors;
+    TArray<FProcMeshTangent> Tangents;
 
+    // Итерируемся по объему, создавая кубы
+    for (int x = 0; x < Resolution; ++x)
+    {
+        for (int y = 0; y < Resolution; ++y)
+        {
+            for (int z = 0; z < Resolution; ++z)
+            {
+                // Определяем позицию текущего куба
+                FVector position(x * cubeSize, y * cubeSize, z * cubeSize);
 
-	// Load the basic Engine sphere mesh
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMesh(TEXT("/Engine/BasicShapes/Sphere.Sphere"));
-	if (SphereMesh.Succeeded())
-	{
-		MeshComponent->SetStaticMesh(SphereMesh.Object);
-	}
+                //Проверяем, находится ли текущий куб внутри поверхности
+                if (DensityFunction(position, Radius) > 0)
+                {
+                    // Если куб внутри поверхности, создаем примитивный куб (это упрощенный пример)
+                    int startVertex = Vertices.Num();
+                    Vertices.Add(position);
+                    Vertices.Add(position + FVector(cubeSize, 0, 0));
+                    Vertices.Add(position + FVector(cubeSize, cubeSize, 0));
+                    //Добавляем остальные вершины упрощенного куба
 
+                    // Добавляем треугольники, образующие грань куба
+                    Triangles.Add(startVertex);
+                    Triangles.Add(startVertex + 1);
+                    Triangles.Add(startVertex + 2);
+                    //Добавляем другие треугольники
 
-	// Load default engine material
-	static ConstructorHelpers::FObjectFinder<UMaterial> Material(TEXT("/Engine/BasicShapes/BasicShapeMaterial.BasicShapeMaterial"));
-	if (Material.Succeeded())
-	{
-		MeshComponent->SetMaterial(0, Material.Object);
-	}
+                }
+            }
+        }
+    }
+
+    // Создаем секцию меша, чтобы он отобразился в игре
+    MeshComponent->CreateMeshSection(0, Vertices, Triangles, Normals, UVs, VertexColors, Tangents, true);
 }
 
-
-void AMarching_Cubes::BeginPlay()
+// Функция, определяющая плотность в точке.  В данном случае, это шар.
+float UMarching_Cubes::DensityFunction(FVector Point, float Radius)
 {
-	Super::BeginPlay();
-	// Sphere will be visible as soon as you drop this actor into the level.
+    // Расстояние от начала координат
+    float distance = Point.Size();
+    // Если меньше радиуса - внутри "шара"
+    return Radius - distance;
 }
--- Source/Spore_2/Public/Marching_Cubes.h
+++ Source/Spore_2/Public/Marching_Cubes.h
@@ -1,31 +1,25 @@
 // Marching_Cubes.h
 #pragma once
 
-
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "UObject/NoExportTypes.h"
+#include "ProceduralMeshComponent.h"
 #include "Marching_Cubes.generated.h"
 
-
 UCLASS()
-class SPORE_2_API AMarching_Cubes : public AActor
+class SPORE_2_API UMarching_Cubes : public UObject
 {
-	GENERATED_BODY()
-
+    GENERATED_BODY()
 
 public:
-	AMarching_Cubes();
-
-
-protected:
-	virtual void BeginPlay() override;
-
+    // Функция для создания меша Marching Cubes
+    UFUNCTION(BlueprintCallable, Category = "MarchingCubes")
+    void GenerateMesh(UProceduralMeshComponent* MeshComponent, float Radius, int Resolution);
 
 private:
-	UPROPERTY(VisibleAnywhere)
-	UStaticMeshComponent* MeshComponent;
+    // Функция для вычисления значения плотности в точке
+    float DensityFunction(FVector Point, float Radius);
 };
-
 
 
 
