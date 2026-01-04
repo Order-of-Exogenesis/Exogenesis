2b5af217d8cdc37604019d53211fd702df5962a9
Quintessence <82589146+offfelldsshift@users.noreply.github.com>
2026-01-04 15:17:58 +0700

    update

---
+++ Source/Spore_2/Private/GravitationalMovement.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GravitationalMovement.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UGravitationalMovement : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Spore_2/Public/PlanetaryMovement.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PlanetaryMovement.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPORE_2_API UPlanetaryMovement : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
